30f19e23952425ad44c98962f718d841ae7572c1
Yuanjie Li <leo.liyj@gmail.com>
2018-06-28 00:25:38 -0700

    working on reducer/index.js

--- src/containers/searchBar.js
+++ src/containers/searchBar.js
@@ -2,12 +2,35 @@ import React, { Component }from 'react';
 
 import '../index.css';
 export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state= {term: ''};
+        this.onInputChange = this.onInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    onInputChange(e) {
+        this.setState({term: e.target.value});
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+    }
+
     render() {
         return (
-                <form class="form-inline">
-                    <label class="sr-only" for="inlineFormInput">Name</label>
-                    <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0 col-sm-9" id="inlineFormInput" placeholder="Type a Pokemon Name!" />
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                <form className="form-inline" onSubmit={this.handleSubmit}>
+                    <label className="sr-only" htmlFor="inlineFormInput">PokemonName</label>
+                    <input 
+                        type="text" 
+                        className="form-control mb-2 mr-sm-2 mb-sm-0 col-sm-9" 
+                        id="inlineFormInput" 
+                        placeholder="Type a Pokemon Name!" 
+                        value={this.state.term}
+                        onChange={this.onInputChange}
+                        />
+                    <button type="submit" className="btn btn-primary">Search</button>
                 </form>
         )
     }
